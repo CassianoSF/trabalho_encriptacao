d8b8725da79cd6c1ad4a0ce710d1aac7d1056e22
CassianoSF <cassianosfranco@gmail.com>
2018-10-01 11:19:43 -0300

    AES e DES

--- src/App.js
+++ src/App.js
@@ -5,28 +5,35 @@ import { Form, Input, Row, Col, Button, Card } from 'antd';
 const FormItem = Form.Item;
 const InputGroup = Input.Group;
 
-var aesjs = require('aes-js');
-
-var key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+var CryptoJS = require("crypto-js");
 
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-      encriptar: '',
-      desencriptar: '',
-      
-      textoEncriptado: '',
-      textoDesencriptado: '',
-      
-      bytesEncriptados: undefined,
-      bytesDesencriptados: undefined,
+      aes_input_encriptar: '',
+      aes_output_encriptar: '',
+      aes_input_decriptar: '',
+      aes_output_decriptar: '',
+      aes_chave: 'Chave123',
+      aes_tempo_encriptar: 0,
+      aes_tempo_decriptar: 0,
+
+      des_input_encriptar: '',
+      des_output_encriptar: '',
+      des_input_decriptar: '',
+      des_output_decriptar: '',
+      des_chave: 'Chave123',
+      des_tempo_encriptar: 0,
+      des_tempo_decriptar: 0,
     }
 
     this.onChange = this.onChange.bind(this)
-    this.onEncriptar = this.onEncriptar.bind(this)
-    this.onDesencriptar = this.onDesencriptar.bind(this)
+    this.onEncriptarAES = this.onEncriptarAES.bind(this)
+    this.onDesencriptarAES = this.onDesencriptarAES.bind(this)
+    this.onEncriptarDES = this.onEncriptarDES.bind(this)
+    this.onDesencriptarDES = this.onDesencriptarDES.bind(this)
   }
 
   onChange(e){
@@ -34,28 +41,59 @@ class App extends Component {
       [e.target.name]: e.target.value
     })
   }
-  onEncriptar(){
-    let text = this.state.encriptar
-    let textBytes = aesjs.utils.utf8.toBytes(text);
-    let aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-    console.log(aesCtr)
-    let encryptedBytes = aesCtr.encrypt(textBytes);
-    let encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+  onEncriptarAES(){
+    let texto = this.state.aes_input_encriptar
+    let chave = this.state.aes_chave;
+    let start = window.performance.now()
+    let aes_output_encriptar = CryptoJS.AES.encrypt(texto, chave).toString();
+    let finish = window.performance.now()
+    let aes_tempo_encriptar = finish - start
     this.setState({
-      bytesEncriptados: encryptedBytes,
-      textoEncriptado: encryptedHex,
+      aes_output_encriptar: aes_output_encriptar,
+      aes_tempo_encriptar: aes_tempo_encriptar,
     })
   }
 
-  onDesencriptar(){
-    let text = this.state.desencriptar
-    let aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
-    console.log(aesCtr)
-    let decryptedBytes = aesCtr.decrypt(this.state.bytesEncriptados);
-    let decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
+  onDesencriptarAES(){
+    let texto = this.state.aes_input_decriptar
+    let chave = this.state.aes_chave;
+    let start = window.performance.now()
+    let bytes  = CryptoJS.AES.decrypt(texto, chave);
+    let aes_output_decriptar = bytes.toString(CryptoJS.enc.Utf8);
+    let finish = window.performance.now()
+    let aes_tempo_decriptar = finish - start
     this.setState({
-      textoDesencriptado: decryptedText,
-      bytesDesencriptados: decryptedBytes,
+      aes_output_decriptar: aes_output_decriptar,
+      aes_tempo_decriptar: aes_tempo_decriptar,
+    })
+  }
+
+
+  onEncriptarDES(){
+    let texto = this.state.des_input_encriptar
+    let chave = this.state.des_chave;
+    let start = window.performance.now()
+    let des_output_encriptar = CryptoJS.DES.encrypt(texto, chave).toString();
+    let finish = window.performance.now()
+    let des_tempo_encriptar = finish - start
+    this.setState({
+      des_output_encriptar: des_output_encriptar,
+      des_tempo_encriptar: des_tempo_encriptar
+    })
+  }
+
+  onDesencriptarDES(){
+    let texto = this.state.des_input_decriptar
+    let chave = this.state.des_chave;
+    let start = window.performance.now()
+    let bytes  = CryptoJS.DES.decrypt(texto, chave);
+    let des_output_decriptar = bytes.toString(CryptoJS.enc.Utf8);
+    let finish = window.performance.now()
+    let des_tempo_decriptar = finish - start
+    this.setState({
+      des_output_decriptar: des_output_decriptar,
+      des_tempo_decriptar: des_tempo_decriptar
+
     })
   }
 
@@ -63,42 +101,110 @@ class App extends Component {
     return (
       <div>
         <Row>
-          <Col>
-            <Card style={{margin: 30}}>
-              <Form>
-                <FormItem>
-                  <InputGroup compact>
-                    <Input 
-                      style={{width: '80%'}} 
-                      onChange={this.onChange} 
-                      type="text" 
-                      name="encriptar" 
-                      id="encriptar" 
-                      placeholder="Encriptar texto" />
-                        <Button onClick={this.onEncriptar}>Encriptar</Button>
-                  </InputGroup>
+          <Col span={12}>
+            <Card title="AES">
+              <Card style={{margin: 30}}>
+                <Form>
+                  <FormItem>
+                    <InputGroup>
+                      <Input 
+                        style={{width: '100%'}} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        name="aes_chave" 
+                        id="aes_chave"
+                        value={this.state.aes_chave}
+                        placeholder="Chave" />
+                    </InputGroup>
+                  </FormItem>
 
-                </FormItem>
-                <FormItem>
-                  <InputGroup compact>
-                    <Input 
-                      style={{width: '80%'}} 
-                      onChange={this.onChange} 
-                      type="text" 
-                      name="desencriptar" 
-                      id="desencriptar" 
-                      placeholder="Desencriptar texto" />
-                        <Button onClick={this.onDesencriptar}>Desencriptar</Button>
-                  </InputGroup>
+                  <FormItem>
+                    <InputGroup compact>
+                      <Input 
+                        style={{width: '50%'}} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        name="aes_input_encriptar" 
+                        id="aes_input_encriptar" 
+                        placeholder="Encriptar texto" />
+                          <Button onClick={this.onEncriptarAES}>Encriptar</Button>
+                    </InputGroup>
+                  </FormItem>
 
-                </FormItem>
-              </Form>
+                  <FormItem>
+                    <InputGroup compact>
+                      <Input 
+                        style={{width: '50%'}} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        name="aes_input_decriptar" 
+                        id="aes_input_decriptar" 
+                        placeholder="Desencriptar texto" />
+                          <Button onClick={this.onDesencriptarAES}>Desencriptar</Button>
+                    </InputGroup>
+
+                  </FormItem>
+                </Form>
+              </Card>
+              <Card style={{margin: 30}} title={`Texto Encriptado - ${this.state.aes_tempo_encriptar.toFixed(5)}ms`}>
+                {this.state.aes_output_encriptar}
+              </Card>
+              <Card style={{margin: 30}} title={`Texto Desencriptado - ${this.state.aes_tempo_decriptar.toFixed(5)}ms`}>
+                {this.state.aes_output_decriptar}
+              </Card>
             </Card>
-            <Card style={{margin: 30}} title="Texto Encriptado">
-              {this.state.textoEncriptado}
-            </Card>
-            <Card style={{margin: 30}} title="Texto Desencriptado">
-              {this.state.textoDesencriptado}
+          </Col>
+          <Col span={12}>
+            <Card title="DES">
+              <Card style={{margin: 30}}>
+                <Form>
+                  <FormItem>
+                    <InputGroup>
+                      <Input 
+                        style={{width: '100%'}} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        name="aes_chave" 
+                        id="aes_chave"
+                        value={this.state.des_chave}
+                        placeholder="Chave" />
+                    </InputGroup>
+                  </FormItem>
+
+                  <FormItem>
+                    <InputGroup compact>
+                      <Input 
+                        style={{width: '50%'}} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        name="des_input_encriptar" 
+                        id="des_input_encriptar" 
+                        placeholder="Encriptar texto" />
+                          <Button onClick={this.onEncriptarDES}>Encriptar</Button>
+                    </InputGroup>
+                  </FormItem>
+
+                  <FormItem>
+                    <InputGroup compact>
+                      <Input 
+                        style={{width: '50%'}} 
+                        onChange={this.onChange} 
+                        type="text" 
+                        name="des_input_decriptar" 
+                        id="des_input_decriptar" 
+                        placeholder="Desencriptar texto" />
+                          <Button onClick={this.onDesencriptarDES}>Desencriptar</Button>
+                    </InputGroup>
+
+                  </FormItem>
+                </Form>
+              </Card>
+              <Card style={{margin: 30}} title={`Texto Encriptado - ${this.state.des_tempo_encriptar.toFixed(5)}ms`}>
+                {this.state.des_output_encriptar}
+              </Card>
+              <Card style={{margin: 30}} title={`Texto Desencriptado - ${this.state.des_tempo_decriptar.toFixed(5)}ms`}>
+                {this.state.des_output_decriptar}
+              </Card>
             </Card>
           </Col>
         </Row>
